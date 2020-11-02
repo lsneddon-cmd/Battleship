854abcba649d5889b0c4eaafe31828ad62db29d2
Lewis Sneddon <>
2020-11-02 18:01:25 +0000

    Basic game board class

---
+++ Battleship/task/src/battleship/GameBoard.java
@@ -0,0 +1,36 @@
+package battleship;
+
+public class GameBoard {
+    String[][] board = new String[11][21];
+
+    public GameBoard() {
+        board[0][0] = " ";
+        for (int i = 1; i < 21; i += 2) {
+            board[0][i] = " ";
+        }
+        for (int i = 2; i < 21; i += 2) {
+            board[0][i] = String.valueOf(i / 2);
+        }
+        for (int i = 1; i < 11; i++) {
+            for (int j = 1; j < 21; j++) {
+                if (j % 2 == 0) {
+                    board[i][j] = "~";
+                } else {
+                    board[i][j] = " ";
+                }
+            }
+        }
+        for (int i = 1; i < 11; i++) {
+            board[i][0] = String.valueOf((char) (i + 64));
+        }
+    }
+
+    public void printBoard() {
+        for (int i = 0; i < 11; i++) {
+            for (int j = 0; j < 21; j++) {
+                System.out.print(board[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
