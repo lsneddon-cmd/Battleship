61143949f8b143f73841de47c095185407e20763
Lewis Sneddon <>
2020-11-04 06:10:42 +0000

    static testing function

---
+++ Battleship/task/src/battleship/CustomTests.java
@@ -0,0 +1,37 @@
+package battleship;
+
+public class CustomTests {
+    public static void runTestOne() {
+        GameBoard game = new GameBoard();
+        game.printBoard();
+
+        Coord firstCoord = new Coord("A1");
+        Coord secondCoord = new Coord("D1");
+
+        System.out.println("Checking Coords valid");
+        System.out.println(firstCoord.isValidCoord());
+        System.out.println(secondCoord.isValidCoord());
+
+        System.out.println("Checking Coords are vertical");
+        System.out.println(!Coord.areCoordsHorizontal(firstCoord, secondCoord));
+
+        System.out.println("Checking Coords are correct size for Battleship");
+        System.out.println(Coord.isLengthCorrect(firstCoord, secondCoord, new Ship(ShipType.BattleShip)));
+
+        System.out.println("Checking Coords are in ascending order");
+        System.out.println(Coord.doCoordsAscend(firstCoord, secondCoord));
+
+        System.out.println("Checking space for ship function");
+        System.out.println(game.spaceForShip(firstCoord, secondCoord));
+
+        System.out.println("Checking input ship vertically");
+        System.out.println(game.inputShipVertically(firstCoord, secondCoord));
+
+        System.out.println("Printing updated game board");
+        game.printBoard();
+
+        System.out.println("Re-checking space for ship function returns false when no space");
+        System.out.print("Expect False: ");
+        System.out.println(game.spaceForShip(firstCoord, secondCoord));
+    }
+}
--- Battleship/task/src/battleship/Main.java
+++ Battleship/task/src/battleship/Main.java
@@ -4,32 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         // Write your code here
-        GameBoard game = new GameBoard();
-        game.printBoard();
-
-        Coord firstCoord = new Coord("A1");
-        Coord secondCoord = new Coord("D1");
-
-        System.out.println("Checking Coords valid");
-        System.out.println(firstCoord.isValidCoord());
-        System.out.println(secondCoord.isValidCoord());
-
-        System.out.println("Checking Coords are vertical");
-        System.out.println(!Coord.areCoordsHorizontal(firstCoord, secondCoord));
-
-        System.out.println("Checking Coords are correct size for Battleship");
-        System.out.println(Coord.isLengthCorrect(firstCoord, secondCoord, new Ship(ShipType.BattleShip)));
-
-        System.out.println("Checking Coords are in ascending order");
-        System.out.println(Coord.doCoordsAscend(firstCoord, secondCoord));
-
-        System.out.println("Checking space for ship function");
-        System.out.println(game.spaceForShip(firstCoord, secondCoord));
-
-        System.out.println("Checking input ship vertically");
-        System.out.println(game.inputShipVertically(firstCoord, secondCoord));
-
-        System.out.println("Printing updated game board");
-        game.printBoard();
+        CustomTests.runTestOne();
     }
 }
